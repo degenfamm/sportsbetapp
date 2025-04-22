7bf86905986bf13aa9dd96700c3e817c8596ac6a
degenfamm <joshuawhite2426@gmail.com>
2025-04-22 09:40:15 -0700

    Create idk.py

---
+++ idk.py
@@ -0,0 +1,6 @@
+import shutil
+
+# Zip the entire project directory for GitHub upload
+shutil.make_archive("sports_betting_model_full", 'zip', "sports_betting_model")
+
+"sports_betting_model_full.zip created for GitHub upload."
